b876176ffcb816fac08ebf951862441e7a4f30e4
VaNRoZ <giga1345@gmail.com>
2021-11-07 20:49:16 +0200

    007

--- Galaxy/Sun-Earth-Moon.cpp
+++ Galaxy/Sun-Earth-Moon.cpp
@@ -40,7 +40,7 @@ struct Body
 		pos.y = y;
 	}
 
-	void PullBy(const Body& other)
+	void PulledBy(const Body& other)
 	{
 		const float G = 1.0f;
 		float dist = sqrt((pos - other.pos)*(pos - other.pos));
@@ -67,4 +67,41 @@ int main()
 	constexpr float dt = 1.0 / 100.0f;
 	constexpr float r = 5.5f, R = 30.0f;
 
+	Body Sun(1000.0f, 7.0f);
+	Body Earth(1000.0f, 2.0f);
+	Body Moon(1.0f, 1.2f);
+
+	Sun.pos = { 0.0f, 0.0f };
+	Sun.vel = { 0.0f, 0.0f };
+
+	Earth.pos = { R, 0.0f };
+	Earth.vel = { 0.0f, sqrt(Sun.m / R) };
+
+	Moon.pos = { R, r };
+	Moon.vel = { sqrt(Earth.m / r), Earth.vel.y };
+
+	while (true)
+	{
+		scr.Clear();
+
+		Moon.PulledBy(Earth);
+		Moon.PulledBy(Sun);
+		Earth.PulledBy(Moon);
+		Earth.PulledBy(Sun);
+		Sun.PulledBy(Moon);
+		Sun.PulledBy(Earth);
+
+		Moon.Update(dt);
+		Earth.Update(dt);
+		Sun.Update(dt);
+
+		scr.Position(Sun.pos.x, Sun.pos.y);
+
+		Plot(Moon, scr);
+		Plot(Earth, scr);
+		Plot(Sun, scr);
+
+		scr.Draw();
+	}
+	return 0;
 }
